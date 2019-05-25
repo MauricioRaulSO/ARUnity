177056a32ca54e2307a69c4c6db6598c53b6239c
Christian Ceciliano <cristiancehi@gmail.com>
2019-05-25 02:38:10 -0500

    Prototype

---
+++ Assets/Scripts/ARTapToPlace.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System;
+
+public class ARTapToPlace : MonoBehaviour
+{
+    public GameObject placementIndicator;
+
+    private ARRaycastManager arRaycastManager;
+    private ARSessionOrigin arOrigin;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        arRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.
+                SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        } else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        Vector2 screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5F, 0.5F));
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        arRaycastManager.Raycast(screenCenter, hits, TrackableType.All);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+        }
+    }
+}
