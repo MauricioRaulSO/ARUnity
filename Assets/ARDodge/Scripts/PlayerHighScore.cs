94fbe55f5054e6e6ce4582975f2cb4c6cc1bcdb8
MauricioRaulSO <mauricio.serrano@saei.mx>
2019-05-29 23:25:35 -0500

    Agregado High Score

---
+++ Assets/ARDodge/Scripts/PlayerHighScore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHighScore : MonoBehaviour
+{
+    public int Highest;
+
+    void setHighScore(int Nscore)
+    {
+        if (Highest < Nscore)
+        {
+            Highest = Nscore;
+        }
+    }
+}
